cc7d334bae659fdcd0caec7b9be0b399f2fa9bc9
s-debug-alt <smeyer29@amarillocollege.com>
2025-04-14 19:23:13 -0500

    Update errorh.js

--- c_project2_meyer/errorh.js
+++ c_project2_meyer/errorh.js
@@ -1,4 +1,3 @@
-
 // validation for submit
 document.getElementById('job-application-form').addEventListener('submit', function(event){
 event.preventDefault(); // prevent form from submition
@@ -7,12 +6,22 @@ event.preventDefault(); // prevent form from submition
  //message to show messages
   const message = document.getElementById('message');
   message.innerHTML = '';
+  
+// block domain pattern
+  const blocked = /@(gmail|yahoo)\.com$/i;
+  
   try { 
   //email validation
   if (!email.includes('@') || !email.includes('.')) { throw new Error('not valid email');}
   
+// block domain check
+	  if  (blocked.test(email)){
+		   message.style.color ="red";
+		message.textContent = "emails from gmail or yahoo is not allowed.";
+			return}
+	
+  
   window.alert("Thank you"); this.submit(); // use keyword to submit form
   }catch (error) { //if fails
   window.alert(error.message);
-  }});
-  
+}});
